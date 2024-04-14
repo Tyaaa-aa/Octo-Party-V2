0eb081b4155efe572cecde4a25607de16b7c3d67
Tya <sirtya@gmail.com>
2024-04-14 19:48:30 +0800

    [feat]: Added Github and About page
    Also added development credits

--- components/IndexComponent.vue
+++ components/IndexComponent.vue
@@ -12,6 +12,12 @@
 			: "dark";
 	}
 
+	const router = useRouter()
+
+	const goToAboutPage = () => {
+		router.push("about")
+	}
+
 	const octoStore = useOctoStore(); // Using the store
 	const userSettings = useSettingStore();
 
@@ -786,6 +792,18 @@
 								</div>
 							</v-list>
 						</v-col>
+						<v-row cols="6" class="d-flex align-center ma-0 mt-5" style="flex-direction: column">
+							<div>
+								<v-btn prepend-icon="mdi-github" stacked variant="plain" color="grey-darken-1" href="https://github.com/Tyaaa-aa/Octo-Party-V2/" target="_blank">
+									Github
+								</v-btn>
+								
+								<v-btn prepend-icon="mdi-information-outline" stacked variant="plain" color="grey-darken-1" @click="goToAboutPage">
+									About
+								</v-btn>
+							</div>
+							<span class="pa-2">Developed by <a href="https://surya.place/" target="_blank">Surya</a> and <a href="https://tya.design/" target="_blank">Tya</a></span>
+						</v-row>
 					</v-row>
 				</v-card>
 			</v-expand-transition>
---
+++ pages/about.vue
@@ -0,0 +1,87 @@
+<template>
+	<v-container>
+		<!-- Logo and Title -->
+		<v-row align="center">
+			<v-col cols="12" class="text-center" style="max-width: 600px; margin: 0 auto;">
+				<img src="/logo.svg" alt="Octo.Party Logo" class="logo mb-4" />
+				<h1 class="display-2">About Octo.Party</h1>
+				<p class="subtitle-1">
+					Octo.Party is a straightforward solution for watching multiple Twitch streams at once. With Octo Party, you can easily create lists of Twitch streams to watch simultaneously, and generate shareable links to share with your friends.
+				</p>
+			</v-col>
+		</v-row>
+
+		<!-- Features -->
+		<v-row>
+			<v-col cols="12" md="6" class="mx-auto">
+				<h2 class="headline">Features</h2>
+				<ul>
+					<li>
+						Multi-Stream Viewing: Watch multiple Twitch streams concurrently
+					</li>
+					<li>
+						Notifications: Get notified when your favorite streamer comes online
+					</li>
+					<li>
+						Create Your Own List: Easily create and manage your list of Twitch
+						streams
+					</li>
+					<li>
+						Streamlined Experience: Enjoy a straightforward and intuitive
+						interface for seamless viewing
+					</li>
+					<li>
+						Shareable Lists: Create shareable links for your list to send your
+						friends or to your other devices
+					</li>
+					<li>
+						No Accounts: No need to sign up or login. Just add your favorite
+						streamers and Octo.Party will save your data locally
+					</li>
+				</ul>
+			</v-col>
+		</v-row>
+
+		<!-- Go Back Button -->
+		<v-row>
+			<v-col cols="12" class="text-center">
+                <v-btn
+					prepend-icon="mdi-chevron-left"
+					variant="elevated"
+					color="deep-purple-darken-1"
+					@click="goToIndexPage"
+				>
+                    Go Back
+				</v-btn>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script setup>
+	import { useRouter } from "vue-router";
+
+	const router = useRouter();
+
+	const goToIndexPage = () => {
+		router.push("/");
+	};
+</script>
+
+<style scoped>
+	.logo {
+		width: 150px;
+		animation: fadeInDown 1s ease-out;
+	}
+
+	@keyframes fadeInDown {
+		from {
+			opacity: 0;
+			transform: translateY(-20px);
+		}
+		to {
+			opacity: 1;
+			transform: translateY(0);
+		}
+	}
+</style>
