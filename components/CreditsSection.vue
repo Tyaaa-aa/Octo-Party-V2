8a35889bb21781996473f0548f995a6bb54a53c8
Tya <sirtya@gmail.com>
2024-04-17 23:41:27 +0800

    [refactor]: Componentize Credits Section

---
+++ components/CreditsSection.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+	const router = useRouter();
+	const goToAboutPage = () => {
+		router.push("about");
+	};
+</script>
+<template>
+	<v-row
+		cols="6"
+		class="d-flex align-center ma-0 mt-5"
+		style="flex-direction: column"
+	>
+		<div>
+			<v-btn
+				prepend-icon="mdi-github"
+				stacked
+				variant="plain"
+				color="grey-darken-1"
+				href="https://github.com/Tyaaa-aa/Octo-Party-V2/"
+				target="_blank"
+			>
+				Github
+			</v-btn>
+
+			<v-btn
+				prepend-icon="mdi-information-outline"
+				stacked
+				variant="plain"
+				color="grey-darken-1"
+				@click="goToAboutPage"
+			>
+				About
+			</v-btn>
+		</div>
+		<span class="pa-2 text-center">
+			Concept by <a href="https://surya.place/" target="_blank">Surya</a> |
+			Developed by <a href="https://tya.design/" target="_blank">Tya</a>
+		</span>
+	</v-row>
+</template>
--- components/IndexComponent.vue
+++ components/IndexComponent.vue
@@ -765,21 +765,7 @@
 								</div>
 							</v-list>
 						</v-col>
-						<v-row cols="6" class="d-flex align-center ma-0 mt-5" style="flex-direction: column">
-							<div>
-								<v-btn prepend-icon="mdi-github" stacked variant="plain" color="grey-darken-1" href="https://github.com/Tyaaa-aa/Octo-Party-V2/" target="_blank">
-									Github
-								</v-btn>
-								
-								<v-btn prepend-icon="mdi-information-outline" stacked variant="plain" color="grey-darken-1" @click="goToAboutPage">
-									About
-								</v-btn>
-							</div>
-							<span class="pa-2 text-center">
-								Concept by <a href="https://surya.place/" target="_blank">Surya</a> |
-								Developed by <a href="https://tya.design/" target="_blank">Tya</a>
-							</span>
-						</v-row>
+						<CreditsSection />
 					</v-row>
 				</v-card>
 			</v-expand-transition>
