1a8e2e13112e8b9218cf49390b629edd4e3e2f8e
Tya <sirtya@gmail.com>
2024-04-25 06:18:49 +0800

    [refactor]: Componentize Toast Message

--- components/IndexComponent.vue
+++ components/IndexComponent.vue
@@ -184,30 +184,7 @@
 		</div>
 		<EmbedsList />
 	</main>
-
-	<v-snackbar
-		:timeout="3000"
-		color="red-darken-1"
-		v-model="globalStore.isError"
-		rounded="pill"
-	>
-		<span class="pr-md-5">{{ globalStore.errorMsg }}</span>
-		<v-btn color="red-lighten-1" variant="flat" @click="globalStore.isError = false">
-			Close
-		</v-btn>
-	</v-snackbar>
-
-	<v-snackbar
-		:timeout="3000"
-		color="deep-purple-accent-4"
-		v-model="globalStore.isSuccess"
-		rounded="pill"
-	>
-		<span class="pr-md-5">{{ globalStore.successMsg }}</span>
-		<v-btn color="deep-purple-accent-2" variant="flat" @click="globalStore.isSuccess = false">
-			Close
-		</v-btn>
-	</v-snackbar>
+	<ToastMessage />
 </template>
 
 <style scoped>
---
+++ components/ToastMessage.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    const globalStore = useGlobalStateStore();
+</script>
+<template>
+	<v-snackbar
+		:timeout="3000"
+		color="red-darken-1"
+		v-model="globalStore.isError"
+		rounded="pill"
+	>
+		<span class="pr-md-5">{{ globalStore.errorMsg }}</span>
+		<v-btn color="red-lighten-1" variant="flat" @click="globalStore.isError = false">
+			Close
+		</v-btn>
+	</v-snackbar>
+
+	<v-snackbar
+		:timeout="3000"
+		color="deep-purple-accent-4"
+		v-model="globalStore.isSuccess"
+		rounded="pill"
+	>
+		<span class="pr-md-5">{{ globalStore.successMsg }}</span>
+		<v-btn color="deep-purple-accent-2" variant="flat" @click="globalStore.isSuccess = false">
+			Close
+		</v-btn>
+	</v-snackbar>
+</template>
